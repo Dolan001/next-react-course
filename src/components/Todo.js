c1899340c97b42779a36af7cf8058e4f89111210
Dolan001 <dolankumarbairagi@gmail.com>
2023-04-06 13:51:28 +0600

    modal, backdrop component added

--- src/components/Todo.js
+++ src/components/Todo.js
@@ -1,13 +1,27 @@
+import { useState } from "react";
+import Modal from "./Modal";
+import Backdrop from "./Backdrop";
+
 function Todo(props) { 
+
+    const [modal, setModal] = useState(false)
+
     function deleteHandler() {
-        console.log('Clicked ' + props.text);
+        setModal(true);
     }
+    function deleteModalHandler(){
+        setModal(false);
+    }
+
     return (
         <div className='card'>
             <h3>{props.text}</h3>
             <div className="actions">
                 <button className="btn" onClick={deleteHandler}>Delete</button>
             </div>
+
+            { modal && <Modal onCancel = {deleteModalHandler} onConfirm = {deleteModalHandler} /> }
+            { modal && <Backdrop onCancel = {deleteModalHandler} /> }
         </div>
     )
  }
